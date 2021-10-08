7c2d1b53d41e8638e20180771e9e0cce62f0ab0d
Yana-Lesina <yanalesina3@gmail.com>
2021-10-08 23:05:37 +0300

    Add to lesson02 2 versions *

--- lesson02/script.js
+++ lesson02/script.js
@@ -7,17 +7,26 @@ console.log(typeof numStr);//проверка типа данных
 console.log(numStr);//вывод строки 
 
 
+
+//ПЕРВЫЙ СПОСОБ ПЕРЕМНОЖЕНИЯ===================
 let mult = 1;//переменная результата умножения
 
 for (let i = 0; i < numStr.length; i++) {
 
   mult *= numStr[i];
   console.log(numStr[i]);
-
 }
+console.log('Результат умножения (циклом for): ' + mult);
+
+//ВТОРОЙ СПОСОБ ПЕРЕМНОЖЕНИЯ===================
+let num2 = 266219;//создание числовой переменной
+num2 = num2 + "";//перевод из числа в строку
+const num2Arr = num2.split("");//разбиваем строку на массив цифр
+const reducer = (previousValue, currentValue) => previousValue * currentValue;
+console.log('Результат умножения (reduce(reducer)):' + num2Arr.reduce(reducer));
 
 
-console.log('Результат умножения: ' + mult);
+
 console.log('Результат возведения в степень: ' + mult**3);
 
 let multStr = String(mult);// преобразование числа в строку 
