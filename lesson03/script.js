36f1d4963616cbc94bf6ff7565a8dfe2b6add0b3
Yana-Lesina <yanalesina3@gmail.com>
2021-10-08 22:40:13 +0300

    Add to lesson03

--- lesson03/script.js
+++ lesson03/script.js
@@ -2,6 +2,7 @@
 
 let lang = prompt('ru or eng?');
 
+//КОНСТРУКЦИЯ IF
 if(lang == 'ru') {
   console.log('Пн, Вт, Ср, Чт, Пт, Сб, Вс');
 
@@ -12,7 +13,7 @@ if(lang == 'ru') {
   console.log('Неправильный ввод');
 }
 
-
+//КОНСТРУКЦИЯ SWITCH 
 switch(lang) {
   case 'ru':
     console.log('Пн, Вт, Ср, Чт, Пт, Сб, Вс');
@@ -26,5 +27,17 @@ switch(lang) {
     console.log('Неправильный ввод');
 }
 
+//МНОГОМЕРНЫЙ МАССИВ 
+const weekArray = [['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'], ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']];
 
+lang === 'ru' ? console.log(weekArray[0]) : console.log(weekArray[1]) //тернарный оператор
 
+lang=='ru' && console.log(weekArray[0])//замыкание
+lang=='eng' && console.log(weekArray[1])
+
+//================================================
+//ЗАДАНИЕ 2
+
+let namePerson = prompt('Как Вас зовут?')
+
+namePerson === 'Артём' ? console.log('Вы директор!') : namePerson === 'Александр' ? console.log('Вы преподаватель!') : console.log('Вы студент!')
