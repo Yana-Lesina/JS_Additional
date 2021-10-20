9d9b4b3c7a8101a944e059c31dbcc346be61946e
Yana-Lesina <yanalesina3@gmail.com>
2021-10-20 22:35:15 +0300

    correct cheking if

--- lesson07/script.js
+++ lesson07/script.js
@@ -6,11 +6,11 @@ const today = new Date();
 
 for (let i = 0; i < 7; i++) {
 
-  if(i + 1 == today.getDay()) {
+  if(i + 1 === today.getDay()) {
     week[i] = '<b>' + week[i] + '</b>'
   }
 
-  if(i == 5 || i == 6) {
+  if(week[i] === 'Суббота' || week[i] === 'Воскресенье') {
     weekOutput.innerHTML += '<i>' + week[i] + '</i>' + '<br>'
   } else {
     weekOutput.innerHTML += week[i] + '<br>'
