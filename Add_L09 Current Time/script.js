8880883d8f33c509c639d7d5081f24fffa8a9aee
Yana-Lesina <yanalesina3@gmail.com>
2021-10-22 19:23:21 +0300

    Add to L09

--- Add_L09 Current Time/script.js
+++ Add_L09 Current Time/script.js
@@ -1,12 +1,36 @@
 'use strict'
 
-let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'] 
-let months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'серьября', 'октября', 'ноября', 'декабря'] 
 const today = new Date()
 let weekOutput = document.getElementById('week')
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'] 
+let months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'] 
 
+
+//добавление нулей к числам первого порядка 
 const pad = function(s) { return (s < 10) ? '0' + s : s; }
 
+//довавление правильного склонения
+const tDecl = function(value, word) {
+    
+    let w = ''
+    if(word === 'час') {
+      w = ['час', 'часа', 'часов']
+    } else if(word === 'мин') {
+      w = [ 'минута', 'минуты', 'минут']
+    } else if(word === 'сек') {
+      w = [ 'секунда', 'секунды', 'секунд']
+    };
+
+
+    const num = value % 10
+    if( value > 10 && value < 20 ) return pad(value) + ' ' + w[2];
+    if( num >= 2 && num <= 4 ) return pad(value) + ' ' + w[1];
+    if(num === 1) return pad(value) + ' ' + w[0];
+    
+    return pad(value) + ' ' + w[2];
+}
+
+//Вывод даты в формате 'Сегодня Вторник, 4 февраля 2020 года, 21 час 5 минут 33 секунды'
 const convertDate1 = function(d) {
   let day = '';
   let month = '';
@@ -23,18 +47,21 @@ const convertDate1 = function(d) {
     } 
   }
 
-  return ['Сегодня',day].join(' ') + ', ' + [pad(d.getDate()),month,d.getFullYear()].join(' ') + ', '
+  return ['Сегодня', day].join(' ') + ', ' 
+        + [pad(d.getDate()),month,d.getFullYear()].join(' ') + ', ' 
+        + [tDecl(d.getHours(), 'час'), tDecl(d.getMinutes(), 'мин'), tDecl(d.getSeconds(), 'сек')].join(' ') + '<br>'
 }
 
-
+//Вывод даты в формате '04.02.2020 - 21:05:33'
 const convertDate2 = function(d) {  
-  return [pad(d.getHours()), pad(d.getMinutes()), pad(d.getSeconds())].join(':') + ' ' + [pad(d.getDate()), pad(d.getMonth() + 1), d.getFullYear()].join('.')  
+  return [pad(d.getHours()), pad(d.getMinutes()), pad(d.getSeconds())].join(':') + ' - ' 
+        + [pad(d.getDate()), pad(d.getMonth() + 1), d.getFullYear()].join('.') + '<br>'
 }
 
-weekOutput.innerHTML = convertDate1(today) + '<br>'
+weekOutput.innerHTML = convertDate1(today) 
 weekOutput.innerHTML += convertDate2(today)
 
-setInterval(function() {
+setInterval(function() { 
   location.reload() 
 }, 1000)
 
