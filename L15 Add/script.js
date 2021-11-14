80ebae7e3fdc349c99edcf31b59493cf55e44b15
Yana-Lesina <yanalesina3@gmail.com>
2021-11-14 20:32:46 +0300

    L15 Add: square movement

--- L15 Add/script.js
+++ L15 Add/script.js
@@ -1,14 +1,15 @@
 'use strict';
 
-const DomElement = function(selector, height, width, bg, fontSize) {
+const DomElement = function(selector, height, width, bg, fontSize, position) {
   this.selector = selector;
   this.height = height;
   this.width = width;
   this.bg = bg;
   this.fontSize = fontSize;
+  this.position = position;
 };
 
-DomElement.prototype.createElemOnPage = function(elemType) {
+DomElement.prototype.createHtmlElem = function(elemType) {
   const newElem = document.createElement(elemType);
 
   if(this.selector[0] === '.') {
@@ -24,55 +25,49 @@ DomElement.prototype.createElemOnPage = function(elemType) {
   newElem.style.cssText = `height: ${this.height};
                            width: ${this.width};
                            background-color: ${this.bg};
-                           font-size: ${this.fontSize}; 
-                           margin: auto;`;
+                           font-size: ${this.fontSize};
+                           position: ${this.position}`;
   
   return newElem;
 };
 
-//ArrowRight
-//ArrowLeft
-//ArrowUp
-//ArrowDown
 
-const move = function(e) {
-  //console.log('move');
-  console.log(e.code);
-  switch (e.code) {
-    case 'ArrowUp':
-      
-      break;
-    case 'ArrowDown':
-      
-      break;
-    case 'ArrowLeft':
-      
-      break;
-    case 'ArrowRight':
-      
-      break;
-  
-    default:
-      break;
-  }
-
-}
-
-
-const elem1 = new DomElement('#block', '100px', '100px', '#f2d335', '35px');
-elem1.position = 'absolute';
-console.log(elem1);
+const elem1 = new DomElement('#block', '100px', '100px', '#f2d335', '14px', 'absolute');
+let newEl1 = elem1.createHtmlElem('div');
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOMContentLoaded");
 
-  const newEl1 = elem1.createElemOnPage('div');
+  //newEl1 = elem1.createElemOnPage('div');
   document.body.append(newEl1);
-  newEl1.textContent = ':)';
-
+  newEl1.textContent = 'ヽ(￣～￣　)ノ';
   console.log(newEl1);
 
   document.addEventListener('keydown', move);
 
 })
 
+const move = function(e) {
+  console.log(e.code);
+  switch (e.code) {
+    case 'ArrowUp':
+      //console.log(`top: ${newEl1.style.top} bottom: ${newEl1.style.bottom}`);
+      newEl1.style.top = (parseInt(newEl1.style.top) || 0) - 10 + 'px'
+
+      break;
+    case 'ArrowDown':
+      //console.log(`top: ${newEl1.style.top} bottom: ${newEl1.style.bottom}`);
+      newEl1.style.top = (parseInt(newEl1.style.top) || 0) + 10 + 'px'
+
+      break;
+    case 'ArrowLeft':
+      newEl1.style.left = (parseInt(newEl1.style.left) || 0) - 10 + 'px'
+      break;
+    case 'ArrowRight':
+      newEl1.style.left = (parseInt(newEl1.style.left) || 0) + 10 + 'px'
+      break;
+  
+    default:
+      break;
+  }
+}
